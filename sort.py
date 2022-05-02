ca476e1f5bb15ad81dd1a24b5b31c8b7c077f5e3
Titouann Wtt <63909350+titouannwtt@users.noreply.github.com>
2022-05-02 17:30:59 +0200

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,36 @@
+################################################################
+
+import subprocess
+import pandas as pd
+import sys, getopt
+import sys
+import telegram_send
+
+##########################
+#   VALEUR PAR DEFAUT
+##########################
+
+csv="/home/moutonneux/bots/backtest/backtest-indicateur/sorted.csv"
+argumentList = sys.argv[1:]
+bestValue=0.0
+try :
+    ancienClassement = pd.read_csv(csv)
+    bestValue=float(ancienClassement['finalBalance'].iloc[0])
+    print(f"BestValue={bestValue}")
+except :
+    print(f"Le fichier {csv} n'a pas été trouvé, il va être créé")
+    pass
+classement = pd.DataFrame([])
+for arg in argumentList:
+    dataframe=pd.read_csv(arg)
+    classement = classement.append(dataframe, ignore_index=True)
+
+classement=classement.sort_values(by=['finalBalance'], ascending=False)
+print(classement)
+classement.to_csv(csv)
+print(f"BestValue={bestValue}")
+print(f"{classement['finalBalance'].iloc[0]}")
+if bestValue<float(classement['finalBalance'].iloc[0]):
+    telegram_send.send(messages=[f"Nouvelle meilleure performance pour l'algo multiples indicators {bestValue}"])
+    print("Nouvelle meilleure performance pour l'algo multiples indicators!")
+    
