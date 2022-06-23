d6a84ba7ae2117611f656231f7f4e87206f02bc8
Titouann Wtt <63909350+titouannwtt@users.noreply.github.com>
2022-06-23 12:15:33 +0200

    Add files via upload

--- sort.py
+++ sort.py
@@ -10,7 +10,8 @@ import telegram_send
 #   VALEUR PAR DEFAUT
 ##########################
 
-csv="/home/moutonneux/bots/backtest/backtest-indicateur/sorted.csv"
+csv="all_results.csv"
+bestValuecsv="best_results.csv"
 argumentList = sys.argv[1:]
 bestValue=0.0
 try :
@@ -26,11 +27,14 @@ for arg in argumentList:
     classement = classement.append(dataframe, ignore_index=True)
 
 classement=classement.sort_values(by=['finalBalance'], ascending=False)
-print(classement)
+classement.drop(classement.columns[classement.columns.str.contains('Unnamed',case = False)],axis = 1, inplace = True)
 classement.to_csv(csv)
+classement = classement[classement.finalBalance >= 1050.0]
+classement.to_csv(bestValuecsv)
+print(classement)
 print(f"BestValue={bestValue}")
 print(f"{classement['finalBalance'].iloc[0]}")
 if bestValue<float(classement['finalBalance'].iloc[0]):
-    telegram_send.send(messages=[f"Nouvelle meilleure performance pour l'algo multiples indicators {bestValue}"])
+    telegram_send.send(messages=[f"Nouvelle meilleure performance pour l'algo multiples indicators {float(classement['finalBalance'].iloc[0])}"])
     print("Nouvelle meilleure performance pour l'algo multiples indicators!")
     
